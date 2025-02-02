d7a495e7ce07eab314911644116187d4e73fce7d
lazytech614 <derupanjan2021@gmail.com>
2025-02-02 13:54:46 +0530

    create category screen

---
+++ app/(routes)/category/[categoryId]/components/MobileFilters.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import React, {useState} from 'react'
+import { Filter as FilterIcon, X } from 'lucide-react';
+
+import { Color, Size } from '@/types';
+import Button from '@/components/ui/button';
+import { Dialog, DialogPanel } from '@headlessui/react';
+import IconButton from '@/components/ui/iconButton';
+import Filter from './Filter';
+
+interface MobileFiltersProps {
+    sizes: Size[];
+    colors: Color[];
+}
+
+const MobileFilters: React.FC<MobileFiltersProps> = ({sizes, colors}) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const onOpen = () => setIsOpen(true)
+  const onClose = () => setIsOpen(false)
+
+  return (
+    <>
+      <Button onClick={onOpen} className='flex items-center gap-x-2 lg:hidden'>
+        <FilterIcon size={20} className='mr-2' /> 
+        Filters
+      </Button>
+      <Dialog open={isOpen} as='div' className='relative z-40 lg:hidden' onClose={onClose}>
+        <div className='fixed inset-0 bg-black bg-opacity-25' />
+        <div className='fixed inset-0 z-40 flex'>
+          <DialogPanel className={'relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl'}>
+            <div className='flex items-center justify-end px-4'>
+              <IconButton 
+                icon={<X size={15} />} 
+                onClick={onClose}
+              />
+            </div>
+            <div className='p-4'>
+            <Filter 
+                valueKey='sizeId'
+                name='Sizes'
+                data={sizes}
+            />
+            <Filter 
+                valueKey='colorId'
+                name='Colors'
+                data={colors}
+            />
+            </div>
+          </DialogPanel>
+        </div>
+      </Dialog>
+    </>
+  )
+}
+
+export default MobileFilters
---
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -0,0 +1,76 @@
+import React from 'react'
+
+import getColors from '@/actions/getColors'
+import getProducts from '@/actions/getProducts'
+import getSingleCategory from '@/actions/getSingleCategory'
+import getSizes from '@/actions/getSizes'
+import Billboard from '@/components/Billboard'
+import Container from '@/components/ui/container'
+import Filter from './components/Filter'
+import MobileFilters from './components/MobileFilters'
+import NoResult from '@/components/ui/noResult'
+import ProductCard from '@/components/ui/productCard'
+
+interface CategoryPageProps {
+    params: {categoryId: string}
+    searchParams: { 
+        colorId: string
+        sizeId: string
+    }
+}
+
+const CategoryPage: React.FC<CategoryPageProps> = async ({
+    params,
+    searchParams
+}) => {
+    const products = await getProducts({
+        categoryId: params.categoryId,
+        colorId: searchParams.colorId,
+        sizeId: searchParams.sizeId
+    })
+    const sizes = await getSizes()
+    const colors = await getColors()
+    const category = await getSingleCategory(params.categoryId)
+    
+  return (
+    <div className='bg-white'>
+        <Container>
+            {/* @ts-ignore */}
+            <Billboard data={category.billboard} />
+            <div className='px-4 sm:px-6 lg:px-8 pb-24'>
+                <div className='lg:grid lg:grid-cols-5 lg:gap-x-8'>
+                    <MobileFilters 
+                        sizes={sizes}
+                        colors={colors}
+                    />
+                    <div className='hidden lg:block'>
+                        <Filter 
+                            valueKey='sizeId'
+                            name='Sizes'
+                            data={sizes}
+                        />
+                        <Filter 
+                            valueKey='colorId'
+                            name='Colors'
+                            data={colors}
+                        />
+                    </div>
+                    <div className='mt-6 lg:col-span-4 lg:mt-0'>
+                        {products.length === 0 && <NoResult />}
+                        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
+                            {products.map((item) => (
+                                <ProductCard
+                                    key={item.id}
+                                    product={item}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Container>
+    </div>
+  )
+}
+
+export default CategoryPage
